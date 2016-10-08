4ed562c8596c07928e8c468de4577f0c10b04cfb
ihommani <ismaelhommani@gmail.com>
2016-10-08 15:41:57 +0200

    Create Average calculator
    On the last thee [month]

---
+++ src/main/java/transformer/BankinToItemAverage.java
@@ -0,0 +1,63 @@
+package transformer;
+
+import model.BankInOutput;
+import model.Transaction;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+/**
+ * Created on 08/10/16.<br/>
+ */
+public class BankinToItemAverage {
+
+    private final Month month;
+
+    private final BankinToItemAverage.BankInputToBankIn bankInputToBankIn = new BankinToItemAverage.BankInputToBankIn();
+
+    public BankinToItemAverage(Month month) {
+        this.month = month;
+    }
+
+    public List<BankInOutput.AverageItem> transform(Map<Integer, String> categoryNameById, List<Transaction> transactions) {
+        return bankInputToBankIn.apply(categoryNameById, transactions);
+    }
+
+    class BankInputToBankIn implements BiFunction<Map<Integer, String>, List<Transaction>, List<BankInOutput.AverageItem>> {
+
+        @Override
+        public List<BankInOutput.AverageItem> apply(Map<Integer, String> categoryNameById, List<Transaction> transactions) {
+
+            return categoryNameById.entrySet().stream()
+                    .map(entry -> {
+                        double averageSpendinOnCategory = transactions.stream()
+                                .map(transaction -> {
+                                    transaction.setDate(LocalDate.parse(transaction.getDate()).plusMonths(transaction.getMove_to_month()).toString());
+                                    return transaction;
+                                })
+                                .filter(transaction -> entry.getKey().equals(transaction.getCategory_id()))
+                                .filter(transaction -> {
+                                    LocalDate transactionDate = LocalDate.parse(transaction.getDate());
+                                    return transactionDate.isBefore(LocalDate.of(transactionDate.getYear(), month, transactionDate.getDayOfMonth()));
+                                })
+                                .filter(transaction -> {
+                                    LocalDate transactionDate = LocalDate.parse(transaction.getDate());
+                                    LocalDate startDate = transactionDate.minusMonths(4);
+                                    return transactionDate.isBefore(LocalDate.of(transactionDate.getYear(), month, transactionDate.getDayOfMonth())) && transactionDate.isAfter(startDate);
+                                })
+                                .mapToDouble(Transaction::getAmount)
+                                .average()
+                                .orElse(0);
+
+                        System.out.println(averageSpendinOnCategory);
+
+                        return new BankInOutput.AverageItem(entry.getKey(), entry.getValue(), averageSpendinOnCategory);
+                    })
+                    .collect(Collectors.toList());
+        }
+    }
+}
---
+++ src/test/java/transformer/BankinToAverageItemTest.java
@@ -0,0 +1,42 @@
+package transformer;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import model.BankInput;
+import model.Category;
+import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Month;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Created on 08/10/16.<br/>
+ */
+public class BankinToAverageItemTest {
+
+    private Gson gson = new GsonBuilder()
+            .disableHtmlEscaping()
+            .setPrettyPrinting()
+            .create();
+
+    @Test
+    public void should_transform_input_into_items_contening_average_values() throws IOException {
+
+        // given
+        BankinToItemAverage underTest = new BankinToItemAverage(Month.JUNE);
+        BufferedReader bf = Files.newBufferedReader(Paths.get(BankinToItemSum.class.getClassLoader().getResource("transformer/inputLevel3.json").getPath()));
+        BankInput bankInput = gson.fromJson(bf, BankInput.class);
+
+        // when
+        Map<Integer, String> categoryNameById = bankInput.getCategories().stream().collect(Collectors.toMap(Category::getId, Category::getName));
+        underTest.transform(categoryNameById, bankInput.getTransactions());
+
+        // then
+
+    }
+}
