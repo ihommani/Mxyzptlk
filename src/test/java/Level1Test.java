111063ba124b363563df55eb7ac989570ee318ed
ihommani <ismaelhommani@gmail.com>
2016-10-08 14:04:10 +0200

    Remove useless case test
    Now these case tests are taken care of the
    InputValidator tests.

--- src/test/java/Level1Test.java
+++ src/test/java/Level1Test.java
@@ -1,5 +1,4 @@
 import org.assertj.core.api.Assertions;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.BufferedReader;
@@ -12,34 +11,6 @@ import java.nio.file.Paths;
  * Created on 06/10/16.<br/>
  */
 public class Level1Test {
-
-    @DataProvider(name = "badInput")
-    public static Object[][] badInput() {
-        return new Object[][]{
-                {new String[]{}},
-                {new String[]{"one", "two", "three"}}
-        };
-    }
-
-    @Test(dataProvider = "badInput", expectedExceptions = IllegalStateException.class, expectedExceptionsMessageRegExp = "Usage: Level1 \\[path_input.json\\] \\[path_output_file.json\\]")
-    public void should_fail_when_no_argument_is_given(String[] args) throws IOException {
-
-        // given
-        // when
-        // then
-        Level1.main(args);
-
-    }
-
-    @Test(expectedExceptions = IllegalStateException.class, expectedExceptionsMessageRegExp = "Bad file path provided:.*")
-    public void should_fail_when_providing_bad_path() throws IOException {
-
-        // given
-        // when
-        // then
-        Level1.main(new String[]{Level1.class.getResource("").getPath()});
-
-    }
 
     @Test
     public void should_generate_output() throws IOException {
